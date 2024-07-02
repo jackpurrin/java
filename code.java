928cd90e86a5d97abd12a0cb14ef4499c9444a23
Jackson Zielinski <jackpurrin@proton.me>
2024-07-02 14:24:17 -0700

    code.java

--- code.java
+++ code.java
@@ -1,1 +1,5 @@
-
+public class code { // Always add a class that is the same name as the project. A class is, to my knowledge, a single concept. Right?
+  public static void main(String[] args) { // I have no clue what this does...
+    System.out.println("Hello World"); // This prints "Hello World" into the console.
+  }
+}
