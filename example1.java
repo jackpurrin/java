f6a105e2dc5e5be38bc9130d9329275a0d2b5193
Jackson Zielinski <jackpurrin@proton.me>
2024-07-02 14:32:14 -0700

    example1.java

---
+++ example1.java
@@ -0,0 +1,11 @@
+public class example1 { // Always add a class that is the same name as the project. A class is, to my knowledge, a single concept. Right?
+  public static void main(String[] args) { // I have no clue what this does...
+    System.out.println("Hello World");  
+    
+    /* This prints "Hello World" into the console.
+       "System" is an pre-defined class that has some useful tools for our programs such as "out"!
+       "out" is a shorter term for output, and outputs well... outputs stuff into the console.
+       "println" is also another shortened term for print line. It's pretty obivous what it does, so no explanation there. */
+    
+  }
+}
