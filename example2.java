f3bf5e8eaa5a957a98927d01d5b4ff566fa39264
Jackson Zielinski <jackpurrin@proton.me>
2024-07-02 14:39:40 -0700

    example2.java

--- example2.java
+++ example2.java
@@ -1,10 +1,10 @@
 public class HideAndSeek { // Already explained in example 1.
   public static void main(String[] args) { // Still no clue lol.
     System.out.println("Let's play hide and seek."); // Prints out a line that says "Let's play hide and seek."
-    System.out.print("Three...") // Simmilar to "println" but instead of printing a whole line, it prints all into one line. This combined with the previous line states "Let's play hide and seek. Three. Two... One..." in the console.
-    System.out.print("Two...")
-    System.out.print("One...")
-
+    System.out.print("Three..."); // Simmilar to "println" but instead of printing a whole line, it prints all into one line. This combined with the previous line states "Let's play hide and seek. Three. Two... One..." in the console.
+    System.out.print(" Two...");
+    System.out.println(" One...");
+    System.out.println("Ready or not, here I come!");
 
   }
 }
